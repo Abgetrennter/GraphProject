8d8d1bf8ec135b157a6acad9e2a09a5cf3d16a3b
Abget-phi <Abget-phi@outlook.com>
2020-07-05 11:05:38 +0800

    basic finished

--- main.c
+++ main.c
@@ -15,35 +15,41 @@ int main(int argc, char const *argv[])
 {
     int n, c;
 
+    //printf("%s\n", argv[1]);
+
     pointpath u_to_v;
 
-    if(strcmp(argv[0], "-h")! || strcmp(argv[0], "--help")!)
+    if(!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))
     {
         printf("help\n"); //help-fun
     }
-    else if(strcmp(argv[0], "-g")! || strcmp(argv[0], "-gragh")!)
+    else if(!strcmp(argv[1], "-g") || !strcmp(argv[1], "-gragh"))
     {
-        strcpy(file_name, argv[1]);
+        strcpy(file_name, argv[2]);
 
         //fun of zhaotu
 
-        if(strcmp(argv[2], "-sp")! || strcmp(argv[2], "--shortestpath"))
+        if(!strcmp(argv[3], "-sp") || !strcmp(argv[3], "--shortestpath"))
         {
-            strcpy(u_to_v.search_path, argv[3]);
+            strcpy(u_to_v.search_path, argv[4]);
 
-            u_to_v.u = atoi(argv[5]);
-            u_to_v.v = atoi(argv[7]);
+            u_to_v.u = atoi(argv[6]);
+            u_to_v.v = atoi(argv[8]);
 
-            u_to_v.path = shortestpath(u_to_v.u, u_to_v.v, u_to_v.search_path, file_name);
+            //u_to_v.path = shortestpath(u_to_v.u, u_to_v.v, u_to_v.search_path, file_name);
 
-            for(int i = 0; ; ++i)
+            /*for(int i = 0;; ++i)
             {
-                /* code */
-            }
+                /* code
+            } */
 
         }
     }
+    else if(!strcmp(argv[1], "-j"))
+    {
+        print_st();
+    }
 
-    print_st();
+    //
     return 0;
 }
