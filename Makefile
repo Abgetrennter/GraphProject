e147a0adeb4c4835d027b6a1ce378386d8b72dde
Abget-phi <Abget-phi@outlook.com>
2020-07-05 10:43:56 +0800

    main

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
-search-cli: main.o
-	cc -o search-cli main.o
+search-cli: main.o search.o stats.o text.o
+	gcc -o search-cli main.o search.o stats.o text.o
 
-main.o:
-	cc -c main.c search.c stats.c
+main.o: main.c search.h stats.h text.h
+	gcc -c  main.c
+
+stats.o: stats.c
+	gcc -c stats.c
+
+search.o: search.c
+	gcc -c search.c
+
+text.o: text.c
+	gcc -c text.c
 
 clean:
+	rm search-cli main.o search.o stats.o text.o
 
--- main.c
+++ main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
-#include "text.h"
+#include <string.h>
+#define Maxsize 100
+//#include "text.h"
+char file_name[Maxsize];
 
+typedef  struct $
+{
+    int u;
+    int v;
+    char search_path[Maxsize];
+    char* path;
+} pointpath;
 int main(int argc, char const *argv[])
 {
+    int n, c;
+
+    pointpath u_to_v;
+
+    if(strcmp(argv[0], "-h")! || strcmp(argv[0], "--help")!)
+    {
+        printf("help\n"); //help-fun
+    }
+    else if(strcmp(argv[0], "-g")! || strcmp(argv[0], "-gragh")!)
+    {
+        strcpy(file_name, argv[1]);
+
+        //fun of zhaotu
+
+        if(strcmp(argv[2], "-sp")! || strcmp(argv[2], "--shortestpath"))
+        {
+            strcpy(u_to_v.search_path, argv[3]);
+
+            u_to_v.u = atoi(argv[5]);
+            u_to_v.v = atoi(argv[7]);
+
+            u_to_v.path = shortestpath(u_to_v.u, u_to_v.v, u_to_v.search_path, file_name);
+
+            for(int i = 0; ; ++i)
+            {
+                /* code */
+            }
+
+        }
+    }
+
     print_st();
     return 0;
 }
